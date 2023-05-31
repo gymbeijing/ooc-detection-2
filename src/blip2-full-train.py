4390f78ce3b3e1bff4fc03a25feb0e175a35d902
gymbeijing <gym_beijing@outlook.com>
2023-05-31 17:23:17 +0100

    Updated

--- src/blip2-full-train.py
+++ src/blip2-full-train.py
@@ -35,7 +35,7 @@ logging.basicConfig(
 # # Define the Dataset class
 
 class TwitterCOMMsDataset(Dataset):
-    def __init__(self, csv_path, img_dir, seen_topics=['military', 'covid', 'climate'], few_shot_topic=[]):
+    def __init__(self, csv_path, img_dir):
         """
         Args:
             csv_path (string): Path to the {train_completed|val_completed}.csv file.
@@ -47,27 +47,6 @@ class TwitterCOMMsDataset(Dataset):
         self.df['exists'] = self.df['filename'].apply(lambda filename: os.path.exists(os.path.join(img_dir, filename)))
         delete_row = self.df[self.df["exists"]==False].index
         self.df = self.df.drop(delete_row)
-        
-        if 'military' in few_shot_topic:
-            self.df['is_military'] = self.df['topic'].apply(lambda topic: 'military' in topic)
-            delete_row = self.df[self.df["is_military"]==True].index
-            #keep_row = self.df[self.df["is_military"]==True].sample(n=5, random_state=42).index   # the few shots
-            #delete_row = np.array(list(set(delete_row)-set(keep_row)))
-            self.df = self.df.drop(delete_row)
-
-        if 'covid' in few_shot_topic:
-            self.df['is_covid'] = self.df['topic'].apply(lambda topic: 'covid' in topic)
-            delete_row = self.df[self.df["is_covid"]==True].index
-            #keep_row = self.df[self.df["is_covid"]==True].sample(n=5, random_state=42).index   # the few shots
-            #delete_row = np.array(list(set(delete_row)-set(keep_row)))
-            self.df = self.df.drop(delete_row)
-
-        if 'climate' in few_shot_topic:
-            self.df['is_climate'] = self.df['topic'].apply(lambda topic: 'climate' in topic)
-            delete_row = self.df[self.df["is_climate"]==True].index
-            #keep_row = self.df[self.df["is_climate"]==True].sample(n=5, random_state=42).index   # the few shots
-            #delete_row = np.array(list(set(delete_row)-set(keep_row)))
-            self.df = self.df.drop(delete_row)
     
     def __len__(self):
         return len(self.df)
@@ -82,9 +61,9 @@ class TwitterCOMMsDataset(Dataset):
 
     def __getitem__(self, idx):
         item = self.df.iloc[idx]
-#         caption = item['full_text']
-#         caption = ' '.join(tt.tokenize(caption))
-#         caption = self.remove_punc(self.remove_URL(caption))
+        caption = item['full_text']
+        caption = ' '.join(tt.tokenize(caption))
+        caption = self.remove_punc(self.remove_URL(caption))
         
         img_filename = item['filename']
         topic = item['topic']
@@ -94,16 +73,13 @@ class TwitterCOMMsDataset(Dataset):
         domain = topic.split('_')[0]
         diff = topic.split('_')[1]
         
-#         raw_image = Image.open(os.path.join(self.img_dir, img_filename)).convert('RGB')
-#         image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
-#         text_input = txt_processors["eval"](caption)
-#         sample = {"image": image, "text_input": [text_input]}   # image shape: [1, 3, 224, 224]
+        raw_image = Image.open(os.path.join(self.img_dir, img_filename)).convert('RGB')
+        image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
+        text_input = txt_processors["eval"](caption)
+        sample = {"image": image, "text_input": [text_input]}   # image shape: [1, 3, 224, 224]
 
-#         features_multimodal = model.extract_features(sample, mode="multimodal")   # [1, 32, 768] ??? image and text might mismatch
-#         multimodal_emb = features_multimodal.multimodal_embeds[:, 0, :]   # [1, 768]
-        tensor_filename = img_filename.split('.')[0] + '.pt'
-        target_path = os.path.join(self.img_dir, tensor_filename)
-        multimodal_emb = torch.load(target_path, map_location=device)
+        features_multimodal = model.extract_features(sample, mode="multimodal")   # [1, 32, 768] ??? image and text might mismatch
+        multimodal_emb = features_multimodal.multimodal_embeds[:, 0, :]   # [1, 768]
 
         return {"multimodal_emb": multimodal_emb,
                 "topic": topic, 
@@ -148,7 +124,7 @@ def train(train_iterator, val_iterator, device):
     criterion = nn.CrossEntropyLoss()
     criterion.to(device)
  
-    EPOCHS = 5
+    EPOCHS = 2
     for epoch in range(EPOCHS):
         net.train()
         total_loss = 0
@@ -177,9 +153,9 @@ def train(train_iterator, val_iterator, device):
             num_total += batch_size
             
             if i % 100 == 0:
-                logger.info("Epoch [%d/%d] %d-th batch: Training accuracy: %.3f, loss: %.3f" % (epoch+1, EPOCHS, i, num_correct/num_total, total_loss/num_total))
+                logger.info("Epoch [%d/%d] %d-th batch: Training accuracy: %.2f, loss: %.2f" % (epoch+1, EPOCHS, i, num_correct/num_total, total_loss/num_total))
             
-        logger.info("Epoch [%d/%d]: Training accuracy: %.3f, loss: %.3f" % (epoch+1, EPOCHS, num_correct/num_total, total_loss/num_total))
+        logger.info("Epoch [%d/%d]: Training accuracy: %.2f, loss: %.2f" % (epoch+1, EPOCHS, num_correct/num_total, total_loss/num_total))
                 
         test(net, val_iterator, criterion, device)
 
@@ -190,20 +166,10 @@ def test(net, iterator, criterion, device):
     
     net.eval()
     
-    softmax = nn.Softmax(dim=1)
-    
     with torch.no_grad():
         total_loss = 0
-        num_correct = dict()
-        num_total = dict()
-        num_correct["all"] = 0
-        num_total["all"] = 0
-        num_correct["climate"] = 0
-        num_total["climate"] = 0
-        num_correct["covid"] = 0
-        num_total["covid"] = 0
-        num_correct["military"] = 0
-        num_total["military"] = 0
+        num_correct = 0
+        num_total = 0
         for i, batch in tqdm(enumerate(iterator, 0), desc='iterations'):
             inputs = batch["multimodal_emb"].to(device)
             labels = batch["label"].to(device)
@@ -214,45 +180,18 @@ def test(net, iterator, criterion, device):
             
             total_loss += loss.item()
             
-            top_pred = torch.zeros_like(labels)
-            y_preds = softmax(y_preds)
-#             print(y_preds[:, 0])
-            top_pred[y_preds[:, 1] >= 0.5] = 1
+            _, top_pred = y_preds.topk(1, 1)
             y = labels.cpu()
             batch_size = y.shape[0]
             top_pred = top_pred.cpu().view(batch_size)
             
-#             _, top_pred = y_preds.topk(1, 1)
-#             y = labels.cpu()
-#             batch_size = y.shape[0]
-#             top_pred = top_pred.cpu().view(batch_size)
-            
-#             num_correct += sum(top_pred == y).item()
-#             num_total += batch_size
-
-            num_correct["all"] += sum(top_pred == y).item()
-            num_total["all"] += batch_size
-            
-            # topic-wise performance
-            topic_labels = batch["topic"]
-            
-            topic_list = ["climate", "covid", "military"]
-            
-            for topic in topic_list:
-                inds = []
-                # print(topic_labels)   # class 'list'
-                for j, tl in enumerate(topic_labels):
-                    if topic in tl:
-                        inds.append(j)
-                num_total[topic] += len(inds)
-                inds = np.array(inds)
-                #num_correct[topic] += sum(top_pred[inds] == y[inds]).item()
-                num_correct[topic] += sum(top_pred[inds] == y[inds])
+            num_correct += sum(top_pred == y).item()
+            num_total += batch_size
             
             if i % 100 == 0:
-                logger.info("%d-th batch: Testing accuracy %.3f, loss: %.3f" % (i, num_correct["all"]/num_total["all"], total_loss/num_total["all"]))
-                
-        logger.info("Overall testing accuracy %.3f, climate testing accuracy %.3f, covid testing accuracy %.3f, military testing accuracy %.3f, loss: %.3f" % (num_correct["all"]/num_total["all"], num_correct["climate"]/num_total["climate"], num_correct["covid"]/num_total["covid"], num_correct["military"]/num_total["military"], total_loss/num_total["all"]))
+                logger.info("%d-th batch: Testing accuracy %.2f, loss: %.2f" % (i, num_correct/num_total, total_loss/num_total))
+            
+        logger.info("Testing accuracy %.2f, loss: %.2f" % (num_correct/num_total, total_loss/num_total))
                 
     return
 
@@ -263,18 +202,16 @@ if __name__ == '__main__':
     device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
     logger.info(device)
 
-#     logger.info("Loading the blip 2 model")
-#     # Load the model
-#     model, vis_processors, txt_processors = load_model_and_preprocess(
-#         name = "blip2_feature_extractor", model_type="pretrain", is_eval=True, device=device
-#     )
+    logger.info("Loading the blip 2 model")
+    # Load the model
+    model, vis_processors, txt_processors = load_model_and_preprocess(
+        name = "blip2_feature_extractor", model_type="pretrain", is_eval=True, device=device
+    )
     
-#     tt = TweetTokenizer()
+    tt = TweetTokenizer()
     logger.info("Loading training data")
     train_data = TwitterCOMMsDataset(csv_path='../data/train_completed.csv',
-                                        img_dir='/import/network-temp/yimengg/data/twitter-comms/train/images/train_image_ids',
-                                    seen_topics=['covid', 'climate'],
-                                    few_shot_topic=['military'])   # took ~one hour to construct the dataset
+                                        img_dir='/import/network-temp/yimengg/data/twitter-comms/train/images/train_image_ids')   # took ~one hour to construct the dataset
     logger.info(f"Found {train_data.__len__()} items in training data")
     
     logger.info("Loading valid data")
--- src/clip-few-shot-copy.py
+++ src/clip-few-shot-copy.py
@@ -20,10 +20,12 @@ import torch.utils.data as data
 from torch import optim
 from torch.autograd import Variable
 import numpy as np
+from sentence_transformers import util
 from nltk.tokenize import TweetTokenizer
 import re, string
 
 import logging
+import numpy as np
 
 # Logger
 logger = logging.getLogger()
@@ -39,7 +41,8 @@ class TwitterCOMMsDataset(Dataset):
         """
         Args:
             csv_path (string): Path to the {train_completed|val_completed}.csv file.
-            image_folder_dir (string): Directory containing the images
+            image_folder_dir (string): Directory containing the images.
+            selected_topic (string): Topic
         """
         self.df = pd.read_csv(csv_path, index_col=0)
         self.img_dir = img_dir
@@ -54,6 +57,7 @@ class TwitterCOMMsDataset(Dataset):
             #keep_row = self.df[self.df["is_military"]==True].sample(n=5, random_state=42).index   # the few shots
             #delete_row = np.array(list(set(delete_row)-set(keep_row)))
             self.df = self.df.drop(delete_row)
+#        print(len(delete_row))
 
         if 'covid' in few_shot_topic:
             self.df['is_covid'] = self.df['topic'].apply(lambda topic: 'covid' in topic)
@@ -61,6 +65,7 @@ class TwitterCOMMsDataset(Dataset):
             #keep_row = self.df[self.df["is_covid"]==True].sample(n=5, random_state=42).index   # the few shots
             #delete_row = np.array(list(set(delete_row)-set(keep_row)))
             self.df = self.df.drop(delete_row)
+#        print(len(delete_row))
 
         if 'climate' in few_shot_topic:
             self.df['is_climate'] = self.df['topic'].apply(lambda topic: 'climate' in topic)
@@ -68,6 +73,7 @@ class TwitterCOMMsDataset(Dataset):
             #keep_row = self.df[self.df["is_climate"]==True].sample(n=5, random_state=42).index   # the few shots
             #delete_row = np.array(list(set(delete_row)-set(keep_row)))
             self.df = self.df.drop(delete_row)
+#        print(len(delete_row))
     
     def __len__(self):
         return len(self.df)
@@ -82,36 +88,53 @@ class TwitterCOMMsDataset(Dataset):
 
     def __getitem__(self, idx):
         item = self.df.iloc[idx]
-#         caption = item['full_text']
-#         caption = ' '.join(tt.tokenize(caption))
-#         caption = self.remove_punc(self.remove_URL(caption))
         
-        img_filename = item['filename']
         topic = item['topic']
         falsified = int(item['falsified'])
         not_falsified = float(not item['falsified'])
+#         label = np.array((falsified, not_falsified))
         label = np.array(falsified)
         domain = topic.split('_')[0]
         diff = topic.split('_')[1]
         
-#         raw_image = Image.open(os.path.join(self.img_dir, img_filename)).convert('RGB')
-#         image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
-#         text_input = txt_processors["eval"](caption)
-#         sample = {"image": image, "text_input": [text_input]}   # image shape: [1, 3, 224, 224]
+        # Preprocess the text
+        caption = item['full_text']
+        caption = ' '.join(tt.tokenize(caption))
+        caption = self.remove_punc(self.remove_URL(caption))
+        
+        # Open the image
+        img_filename = item['filename']
+        raw_image = Image.open(os.path.join(self.img_dir, img_filename)).convert('RGB')
+        
+        # Get the multimodal embedding
+        image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
+        text_input = txt_processors["eval"](caption)
+        sample = {"image": image, "text_input": [text_input]}   # image shape: [1, 3, 224, 224]
+        features = model.extract_features(sample)
+        features_image = features.image_embeds   # [1, 512]
+        features_text = features.text_embeds   # [1, 512]
+#         multimodal_emb = torch.cat((features_image, features_text), 1)
+        multimodal_emb = features_image * features_text
+        
+        cos_sim = util.cos_sim(features_text, features_image)
+#         features_image_proj = features_image.image_embeds_proj[:,0,:]   # [1, 256]
+#         features_text_proj = features_text.text_embeds_proj[:,0,:]   # [1, 256]
+        
+#         multimodal_emb = torch.cat((features_image_proj, features_text_proj), 1)
+#         multimodal_emb = features_image_proj * features_text_proj   # [1, 256]
+#         print(multimodal_emb.shape)
 
-#         features_multimodal = model.extract_features(sample, mode="multimodal")   # [1, 32, 768] ??? image and text might mismatch
-#         multimodal_emb = features_multimodal.multimodal_embeds[:, 0, :]   # [1, 768]
-        tensor_filename = img_filename.split('.')[0] + '.pt'
-        target_path = os.path.join(self.img_dir, tensor_filename)
-        multimodal_emb = torch.load(target_path, map_location=device)
+#         similarity = features_image_proj @ features_text_proj.t()
 
         return {"multimodal_emb": multimodal_emb,
                 "topic": topic, 
                 "label": label, 
                 "domain": domain, 
-                "difficulty": diff}
+                "difficulty": diff,
+               "similarity": cos_sim}
         
-
+        
+    
 class Net(nn.Module):
     def __init__(self, in_dim, out_dim=2):
         super(Net, self).__init__()
@@ -137,7 +160,7 @@ def normal_init(m, mean, std):
 
 def train(train_iterator, val_iterator, device):
 
-    net = Net(768)
+    net = Net(512)
     net.cuda()
     net.train()
     net.weight_init(mean=0, std=0.02)
@@ -147,6 +170,8 @@ def train(train_iterator, val_iterator, device):
     
     criterion = nn.CrossEntropyLoss()
     criterion.to(device)
+    
+    softmax = nn.Softmax(dim=1)
  
     EPOCHS = 5
     for epoch in range(EPOCHS):
@@ -168,7 +193,10 @@ def train(train_iterator, val_iterator, device):
             
             total_loss += loss.item()
             
-            _, top_pred = y_preds.topk(1, 1)
+            top_pred = torch.zeros_like(labels)
+            y_preds = softmax(y_preds)
+#             print(y_preds[:, 0])
+            top_pred[y_preds[:, 1] >= 0.5] = 1
             y = labels.cpu()
             batch_size = y.shape[0]
             top_pred = top_pred.cpu().view(batch_size)
@@ -222,14 +250,6 @@ def test(net, iterator, criterion, device):
             batch_size = y.shape[0]
             top_pred = top_pred.cpu().view(batch_size)
             
-#             _, top_pred = y_preds.topk(1, 1)
-#             y = labels.cpu()
-#             batch_size = y.shape[0]
-#             top_pred = top_pred.cpu().view(batch_size)
-            
-#             num_correct += sum(top_pred == y).item()
-#             num_total += batch_size
-
             num_correct["all"] += sum(top_pred == y).item()
             num_total["all"] += batch_size
             
@@ -251,7 +271,7 @@ def test(net, iterator, criterion, device):
             
             if i % 100 == 0:
                 logger.info("%d-th batch: Testing accuracy %.3f, loss: %.3f" % (i, num_correct["all"]/num_total["all"], total_loss/num_total["all"]))
-                
+            
         logger.info("Overall testing accuracy %.3f, climate testing accuracy %.3f, covid testing accuracy %.3f, military testing accuracy %.3f, loss: %.3f" % (num_correct["all"]/num_total["all"], num_correct["climate"]/num_total["climate"], num_correct["covid"]/num_total["covid"], num_correct["military"]/num_total["military"], total_loss/num_total["all"]))
                 
     return
@@ -263,18 +283,19 @@ if __name__ == '__main__':
     device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
     logger.info(device)
 
-#     logger.info("Loading the blip 2 model")
-#     # Load the model
-#     model, vis_processors, txt_processors = load_model_and_preprocess(
-#         name = "blip2_feature_extractor", model_type="pretrain", is_eval=True, device=device
-#     )
+    logger.info("Loading the clip model")
+    # Load the model
+    model, vis_processors, txt_processors = load_model_and_preprocess(
+        name = "clip_feature_extractor", model_type="ViT-B-32", is_eval=True, device=device
+    )
+    # Load the tokenizer
+    tt = TweetTokenizer()
     
-#     tt = TweetTokenizer()
     logger.info("Loading training data")
     train_data = TwitterCOMMsDataset(csv_path='../data/train_completed.csv',
-                                        img_dir='/import/network-temp/yimengg/data/twitter-comms/train/images/train_image_ids',
-                                    seen_topics=['covid', 'climate'],
-                                    few_shot_topic=['military'])   # took ~one hour to construct the dataset
+                                     img_dir='/import/network-temp/yimengg/data/twitter-comms/train/images/train_image_ids',
+                                    seen_topics=['covid', 'military'],
+                                    few_shot_topic=['climate'])   # took ~one hour to construct the dataset
     logger.info(f"Found {train_data.__len__()} items in training data")
     
     logger.info("Loading valid data")
@@ -282,7 +303,7 @@ if __name__ == '__main__':
                                    img_dir='/import/network-temp/yimengg/data/twitter-comms/images/val_images/val_tweet_image_ids')
     logger.info(f"Found {val_data.__len__()} items in valid data")
     
-    BATCH_SIZE = 64
+    BATCH_SIZE = 32
     train_iterator = data.DataLoader(train_data, 
                                      shuffle=True, 
                                      batch_size=BATCH_SIZE)
@@ -293,6 +314,5 @@ if __name__ == '__main__':
     logger.info("Start training the model")
     
     net = train(train_iterator, val_iterator, device)
-
 
 
