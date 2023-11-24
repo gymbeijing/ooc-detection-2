8dbaadce97a79c0bf1bc267709905db45152d96f
gymbeijing <gym_beijing@outlook.com>
2023-11-24 11:11:19 +0000

    Created taskres.py

---
+++ trainers/taskres.py
@@ -0,0 +1,295 @@
+"""
+Task Residual Tuning
+"""
+
+import os
+import os.path as osp
+from re import template
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.cuda.amp import GradScaler, autocast
+
+from dassl.engine import TRAINER_REGISTRY, TrainerX
+from dassl.metrics import compute_accuracy
+from dassl.utils import load_pretrained_weights, load_checkpoint
+from dassl.optim import build_optimizer, build_lr_scheduler
+
+from clip import clip
+from clip.simple_tokenizer import SimpleTokenizer as _Tokenizer
+from trainers.imagenet_templates import IMAGENET_TEMPLATES, IMAGENET_TEMPLATES_SELECT
+
+torch.backends.cuda.matmul.allow_tf32 = True   # A bool that controls whether TensorFloat-32 tensor cores may be used in matrix multiplications on Ampere or newer GPUs.
+torch.backends.cudnn.benchmark = True   # if True, causes cuDNN to benchmark multiple convolution algorithms and select the fastest
+torch.backends.cudnn.deterministic = False   # if True, causes cuDNN to only use deterministic convolution algorithms
+torch.backends.cudnn.allow_tf32 = True   # A bool that controls where TensorFloat-32 tensor cores may be used in cuDNN convolutions on Ampere or newer GPUs
+
+_tokenizer = _Tokenizer()
+
+CUSTOM_TEMPLATES = {
+    "Twitter-COMMs": "a piece of news in {}."   # {domain}
+}
+
+
+def load_clip_to_cpu(cfg):
+    backbone_name = cfg.MODEL.BACKBONE.NAME
+    url = clip._MODELS[backbone_name]
+    model_path = clip._download(url)
+
+    try:
+        # loading JIT archive
+        model = torch.jit.load(model_path, map_location="cpu").eval()
+        state_dict = None
+
+    except RuntimeError:
+        state_dict = torch.load(model_path, map_location="cpu")
+
+    model = clip.build_model(state_dict or model.state_dict())
+
+    return model
+
+
+class TextEncoder(nn.Module):
+    def __init__(self, clip_model):
+        super().__init__()
+        # Take out clip's components
+        self.transformer = clip_model.transformer
+        self.positional_embedding = clip_model.positional_embedding
+        self.ln_final = clip_model.ln_final
+        self.text_projection = clip_model.text_projection
+        self.dtype = clip_model.dtype
+
+    def forward(self, prompts, tokenized_prompts):
+        # Prompt embedding + clip's positional embedding
+        x = prompts + self.positional_embedding.type(self.dtype)
+        x = x.permute(1, 0, 2)   # NLD -> LND
+        x = self.transformer(x)
+        x = x.permute(1, 0, 2)   # LND -> NLD
+        x = self.ln_final(x).type(self.dtype)
+
+        # x.shape = [batch_size, n_ctx, transformer.width]   n_ctx is the max. number of tokens in an input sequence
+        x = x[torch.arange(x.shape[0]), tokenized_prompts.argmax(dim=-1)] @ self.text_projection   # for each row, what does argmax refer to?
+
+        return x
+
+
+class TaskResLearner(nn.Module):
+    def __init__(self, cfg, classnames, clip_model, base_text_features):
+        super().__init__()
+        self.device = clip_model.dtype
+        self.alpha = cfg.TRAINER.TaskRes.RESIDUAL_SCALE
+        print(">> DCT scale factor: ", self.alpha)
+        self.register_buffer("base_text_features", base_text_features)
+        # Learnable part
+        self.text_feature_residuals = nn.Parameter(torch.zeros_like(base_text_features))
+
+    def forward(self):
+        return self.base_text_featrues + self.alpha * self.text_feature_residuals
+
+
+def _get_base_text_features(cfg, classnames, clip_model, text_encoder):
+    device = next(text_encoder.parameters()).device
+    if clip_model.dtype == torch.float16:
+        text_encoder = text_encoder.cuda()
+
+    dataset = cfg.DATASET.NAME
+
+    TEMPLATES = []
+    TEMPLATES += [CUSTOM_TEMPLATES[dataset]]
+
+    with torch.no_grad():
+        text_embeddings = []
+        for text in classnames:   # text is a domain
+            tokens = clip.tokenize([template.format(text) for template in TEMPLATES])   # Get the tokenized prompt
+            embeddings = clip_model.token_embedding(tokens).type(clip_model.dtype)   # Get the embedding of the tokens
+            if clip_model.dtype == torch.float16:
+                text_embeddings.append(text_encoder(embeddings.cuda(), tokens.cuda()))
+            else:
+                text_embeddings.append(text_encoder(embeddings.cuda(), tokens.cuda()))
+
+    text_embeddings = torch.stack(text_embeddings).mean(1)
+    text_encoder = text_encoder.to(device)
+    return text_embeddings.to(device)
+
+
+def _get_enhanced_base_text_features(cfg, classnames, clip_model, text_encoder, pretraiend_model):
+    device = next(text_encoder.parameters()).device
+    if clip_model.dtype == torch.float16:
+        text_encoder = text_encoder.cuda()
+
+        pretrained_text_projection = torch.load(pretraiend_model)
+
+        state_dict = text_encoder.state_dict()
+        state_dict['text_projection'] = pretrained_text_projection['state_dict']['weight'].t()
+        text_encoder.load_state_dict(state_dict)
+        print(">> Pretrained text encoder loaded!")
+        params = pretrained_text_projection['state_dict']['weight'].size(0) * \
+                 pretrained_text_projection['state_dict']['weight'].size(1)
+        print(">> Text projection parameters: ", params)
+        print(pretrained_text_projection['state_dict'].keys())
+
+    dataset = cfg.DATASET.NAME
+    if dataset == "ImageNet":
+        TEMPLATES = IMAGENET_TEMPLATES_SELECT
+    else:
+        TEMPLATES = []
+    TEMPLATES += [CUSTOM_TEMPLATES[dataset]]
+
+    with torch.no_grad():
+        text_embeddings = []
+        for text in classnames:
+            tokens = clip.tokenize([template.format(text) for template in TEMPLATES])  # tokenized prompts are indices
+            embeddings = clip_model.token_embedding(tokens).type(clip_model.dtype)
+            if clip_model.dtype == torch.float16:
+                text_embeddings.append(text_encoder(embeddings.cuda(), tokens.cuda()))  # not support float16 on cpu
+            else:
+                text_embeddings.append(text_encoder(embeddings.cuda(), tokens.cuda()))
+    text_embeddings = torch.stack(text_embeddings).mean(1)
+    text_encoder = text_encoder.to(device)
+    return text_embeddings.to(device)
+
+
+class CustomCLIP(nn.Module):
+    def __init__(self, cfg, classnames, clip_model):
+        super().__init__()
+        self.image_encoder = clip_model.visual
+        self.logit_scale = clip_model.logit_scale
+        self.dtype = clip_model.dtype   # float16
+        text_encoder = TextEncoder(clip_model)
+        if cfg.TRAINER.TaskRes.ENHANCED_BASE == "none":
+            print(">> Use regular base!")
+            base_text_features = _get_base_text_features(cfg, classnames, clip_model, text_encoder)
+        else:
+            print(">> Use enhanced base!")
+            base_text_features = _get_enhanced_base_text_features(cfg, classnames, clip_model, text_encoder,
+                                                                  cfg.TRAINER.TaskRes.ENHANCED_BASE)
+        self.prompt_learner = TaskResLearner(cfg, classnames, clip_model, base_text_features)
+
+    def forward(self, image):
+        try:
+            image_features = self.image_encoder(image.type(self.dtype))
+        except:
+            image_features = self.image_encoder(image.float())
+
+        text_features = self.prompt_learner()
+
+        # Normalization
+        image_features = image_features / image_features.norm(dim=-1, keepdim=True)
+        text_features = text_features / text_features.norm(dim=-1, keepdim=True)
+
+        logit_scale = self.logit_scale.exp()
+        logits = logit_scale * image_features @ text_features.t()   # @: matrix multiplication, * element-wise multiplication
+
+        return logits
+
+
+@TRAINER_REGISTRY.register()
+class TaskRes(TrainerX):
+    """Context Optimization (TaskRes).
+
+        Task Residual for Tuning Vision-Language Models
+        https://arxiv.org/abs/2211.10277
+    """
+
+    def check_cfg(self, cfg):
+        assert cfg.TRAINER.TaskRes.PREC in ["fp16", "fp32", "amp"]
+
+    def build_model(self):
+        cfg = self.check_cfg
+        classnames = self.dm.dataset.classnames
+
+        print(f"Loading CLIP (backbone: {cfg.MODEL.BACKBONE.NAME})")
+        clip_model = load_clip_to_cpu(cfg)
+
+        if cfg.TRAINER.TaskRes.PREC == "fp32" or cfg.TRAINER.TaskRes.PREC == "amp":
+            clip_model.float()
+
+        print("Building custom CLIP")
+        self.model = CustomCLIP(cfg, classnames, clip_model)
+
+        print("Turning off gradients in both the image and the text encoder")
+        for name, param in self.model.named_parameters():
+            if "prompt_learner" not in name:
+                param.requires_grad_(False)
+            else:
+                print(name)
+
+        if cfg.MODEL.INIT_WEIGHTS:
+            load_pretrained_weights(self.model.prompt_learner, cfg.MODEL.INIT_WEIGHTS)
+
+        self.model.to(self.device)
+        self.model = self.model.float()
+
+        self.optim = build_optimizer(self.model.prompt_learner, cfg.OPTIM)
+        self.sched = build_lr_scheduler(self.optim, cfg.OPTIM)
+        self.register_model("prompt_learner", self.model.prompt_learner, self.optim, self.sched)
+
+        self.scaler = GradScaler() if cfg.TRAINER.TaskRes.PREC =="amp" else None
+
+    def forward_backward(self, batch):
+        image, label = self.parse_batch_train(batch)
+
+        prec = self.cfg.TRAINER.TaskRes.PREC
+        if prec == "amp":
+            with autocast():
+                output = self.model(image)
+                loss = F.cross_entropy(output, label)
+            self.optim.zero_grad()
+            self.scaler.scale(loss).backward()
+            self.scaler.step(self.optim)
+            self.scaler.update()
+        else:
+            output = self.model(image)
+            loss = F.cross_entropy(output, label)
+            self.model_backward_and_update(loss)
+
+        loss_summary = {
+            "loss": loss.item(),
+            "acc": compute_accuracy(output, label)[0].item(),
+        }
+
+        if (self.batch_idx + 1) == self.num_batches:
+            self.update_lr()
+
+        return loss_summary
+
+    def parse_batch_train(self, batch):
+        input = batch["img"]
+        label = batch["label"]
+        input = input.to(self.device)
+        label = label.to(self.device)
+        return input, label
+
+    def load_model(self, directory, epoch=None):
+        if not directory:
+            print("Note that load_model() is skipped as no pretrained model is given")
+            return
+
+        names = self.get_model_names()
+        model_file = "model-best.pth.tar"
+
+        if epoch is not None:
+            model_file = "model-best.pth.tar"
+
+        for name in names:
+            model_path = osp.join(directory, name, model_file)
+
+            if not osp.exists(model_path):
+                raise FileNotFoundError('Model not found at "{}"'.format(model_path))
+
+            checkpoint = load_checkpoint(model_path)
+            state_dict = checkpoint["state_dict"]
+
+            epoch = checkpoint["epoch"]
+
+            if "token_prefix" in state_dict:
+                del state_dict["token_prefix"]
+
+            if "token_suffix" in state_dict:
+                del state_dict["token_suffix"]
+
+            print("Loading weights to {} "  'from "{}" (epoch = {})'.format(name, model_path, epoch))
+            self._models[name].load_state_dict(state_dict, strict=False)
+
+
