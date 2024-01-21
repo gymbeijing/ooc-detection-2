91d3ddc877902bd03257b884a3791738f4efeeef
gymbeijing <gym_beijing@outlook.com>
2024-01-21 23:25:07 +0000

    Multimodal In-context learning

--- src/eval_llava.py
+++ src/eval_llava.py
@@ -72,16 +72,34 @@ def load_queries_and_image_paths(df):
     labels = []
 
     for idx, item in tqdm(df.iterrows(), desc='iterations'):
+        # Prompt 1
         # text = "Answer with yes or no. Does the image match the following text? "
-        text = "Task: News Image-Text Matching Analysis."
-        "Objective: To assess whether the attached image and the provided text description correspond with each other accurately,"
-        "Instructions:"
-        "1. Examine the attached image carefully."
-        "2. Read the text description provided below the image."
-        "3. Determine the degress of alignment between the image content and the text. Consider factors such as the main subjects, background elements, and overall context."
-        "4. Provide your response in a clear 'yes' or 'no' format."
-        "Prompt: Does the following text description accurately represent the content and context of the attached image? Please respond with 'yes' if there is a match and 'no' if there are discrepancies."
-        " Does the image match the following text? "
+
+        # Prompt 2
+        # text = "Task: News Image-Text Matching Analysis."
+        # "Objective: To assess whether the attached image and the provided text description correspond with each other accurately,"
+        # "Instructions:"
+        # "1. Examine the attached image carefully."
+        # "2. Read the text description provided below the image."
+        # "3. Determine the degress of alignment between the image content and the text. Consider factors such as the main subjects, background elements, and overall context."
+        # "4. Provide your response in a clear 'yes' or 'no' format."
+        # "Prompt: Does the following text description accurately represent the content and context of the attached image? Please respond with 'yes' if there is a match and 'no' if there are discrepancies."
+        # " Does the image match the following text? "
+
+        # Prompt 3
+        text = "Below is an instruction that describes the task. Write a response that appropriately completes the request. "
+        "[Instructions]:"
+        "1. Examine the query image (the third attached image) carefully"
+        "2. Read the caption provided carefully"
+        "3. Determine whether the image and the caption are semantically matched. Consider factors such as the main objects, background elements and overall context."
+        "4. Provide your response in a clear ‘yes’ or ‘no’ format."
+        "5. Look at two examples provided with label in the below and get an understanding of the task."
+        "[Example 1]: Take the first attached image and the following caption as the first example with ground true label. [Caption]: Speaker Pelosi and Senate Maj Leader Schumer talking about climate change action now on @cspan"
+        "@cspanwj https://t.co/9vw0qxdk7a [Label]: Yes"
+        "[Example 2]: Take the second attached image and the following caption as the second example with ground true label. [Caption]: Russia and China continue to unabashedly copy military vehicle designs. These pictures are *not* of a Black Hawk, CB90 or A400M..."
+        "(Z-20, Project 03160 and suspected Y-30) https://t.co/vRZtzwxHgn [Label]: Yes"
+        "[Prompt]: Does the following caption accurately represent the content and the context of the query image (the third attached image)? Please respond with ‘yes’ if there is a match and ‘no’ if there are discrepancies identified."
+        "[Caption]: "
         text += item['full_text']  # original caption
         img_filename = item['filename']
         image_path = os.path.join(val_img_dir, img_filename)
